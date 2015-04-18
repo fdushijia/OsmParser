f2780728326537dc6d9e3c414b6e579daf8ac504
fdushijia <fdushijia@163.com>
2015-04-18 18:04:34 +0800

    SPFA完成

--- ShareTaxi/ShareTaxi/RoadInfo.cpp
+++ ShareTaxi/ShareTaxi/RoadInfo.cpp
@@ -3,13 +3,23 @@
 #include <list>
 #include <fstream>
 #include <vector>
+#include <cstring>
+#include <queue>
 #include "Distance.h"
+
 using namespace std;
 
+#define ALL_NODES 500000
+#define MAX_DIST  1000000000
+
 float lat[500000], lon[500000];
+float minDist[500000];
+bool  visit[500000];
+queue<int> q;
 
 struct NeighNode {
 	int id;
+	float dist;
 	NeighNode* next;
 };
 NeighNode* neighbour[500000];
@@ -21,7 +31,7 @@ int n;
 void init() {
 
 	cout<<"Constructing the road..."<<endl;
-	ifstream fin("newNodes_long.txt");
+	ifstream fin("newNodes.txt");
 	string idTemp, latTemp, lonTemp;
 	int i = 0;
 
@@ -37,29 +47,31 @@ void init() {
 	fin.close();
 	
 	cout<<"Constructing..."<<endl;
-	ifstream wayIn("newWays_long.txt");
+	ifstream wayIn("newWays.txt");
 	string ss, tt;
 	int s, t;
 	while (wayIn>>ss>>tt) {
 		s = atoi(ss.c_str());
 		t = atoi(tt.c_str());
 		if (neighbour[s] == NULL) {
-			neighbour[s] = (struct NeighNode *)malloc(sizeof(NeighNode));
-			neighbour[s]->id = t;
+			neighbour[s]       = (struct NeighNode *)malloc(sizeof(NeighNode));
+			neighbour[s]->id   = t;
+			neighbour[s]->dist = Distance::getDist(lat[s], lon[s], lat[t], lon[t]);
 			neighbour[s]->next = NULL;
-			tail[s] = neighbour[s];
+			tail[s]            = neighbour[s];
 		} else {
-			NeighNode *p = (struct NeighNode*)malloc(sizeof(NeighNode));
-			p->id = t;
-			p->next = NULL;
+			NeighNode *p  = (struct NeighNode*)malloc(sizeof(NeighNode)); 
+			p->id         = t;
+			p->dist       = Distance::getDist(lat[s], lon[s], lat[t], lon[t]);
+			p->next       = NULL;
 			tail[s]->next = p;
-			tail[s] = p;
+			tail[s]       = p;
 		}
 	}
 	wayIn.close();		
 }
 
-void recyle() {
+void recycle() {
 	cout<<"End..."<<endl;
 	ofstream fout("neighbourInfo.txt");
 	for (int i = 0; i < n; i++) {
@@ -77,10 +89,48 @@ void recyle() {
 	fout.close();
 }
 
+void initDist(int s) {
+	q.empty();
+	for (int i = 0; i < ALL_NODES; i++) {
+		minDist[i] = MAX_DIST;
+		visit[i]   = false;
+	}
+	minDist[s] = 0;
+}
+
+/**
+ * 因为路网转化成图之后是稀疏图，这里用SPFA算法求单源点最短路径
+ */
+float SPFA(int s, int t) {
+	initDist(s);
+	q.push(s);
+	int k;
+	while (!q.empty()) {
+		k = q.front();
+		q.pop();
+		visit[k] = false;
+		NeighNode *p = neighbour[k];
+		while (p) {
+			if (minDist[k] + p->dist < minDist[p->id]) {
+				minDist[p->id] = minDist[k] + p->dist;
+			}
+			if (!visit[p->id]) {
+				visit[p->id] = true;
+				q.push(p->id);
+			}
+			p = p->next;
+		}
+
+	}
+	return minDist[t];
+}
 
 int main() {
 	init();
-	recyle();
+	
+	cout<<SPFA(46274, 46293)<<endl;
+	recycle();
+
 	return 0;
 }
 
