a6b9c6dea151941597125ddbaf3fb2e617bf21e1
fdushijia <fdushijia@163.com>
2015-04-25 22:27:37 +0800

    reformat

--- ShareTaxi/ShareTaxi/Distance.h
+++ ShareTaxi/ShareTaxi/Distance.h
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define PI				3.1415926
+#define PI              3.1415926
 #define EARTH_RADIUS    6378.137
 
 
@@ -18,9 +18,9 @@ private:
 public:
     static double getDist(double lat1, double lon1, double lat2, double lon2) {
         double radLat1 = radian(lat1);
-		double radLat2 = radian(lat2);
-		double a = radLat1 - radLat2;
-		double b = radian(lon1) - radian(lon2);
+        double radLat2 = radian(lat2);
+        double a = radLat1 - radLat2;
+        double b = radian(lon1) - radian(lon2);
     
 		double dst = 2 * asin((sqrt(pow(sin(a / 2), 2) + cos(radLat1) * cos(radLat2) * pow(sin(b / 2), 2) )));
     
@@ -29,12 +29,5 @@ public:
 		return dst;
     }
 };
-
-
-
-
-
-
-
 
 
