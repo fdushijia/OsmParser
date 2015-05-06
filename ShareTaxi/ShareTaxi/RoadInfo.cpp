ee7ce763825b2c6feadd5a4e98bf0e48931fa649
fdushijia <fdushijia@163.com>
2015-05-06 22:54:02 +0800

    缓存节点到边界点的最短路

--- ShareTaxi/ShareTaxi/RoadInfo.cpp
+++ ShareTaxi/ShareTaxi/RoadInfo.cpp
@@ -8,25 +8,37 @@
 #include <stdlib.h> 
 #include <time.h>
 #include "Distance.h"
+#include <malloc.h>
 
 using namespace std;
 
-#define ALL_NODES 500000      //图中Node的最大值
-#define MAX_DIST  1000000000  //初始最大距离
-#define RATIO     3           //这个系数用来控制SPFA剪枝    
+#define ALL_NODES 435288      //图中Node的最大值
 
-float lat[500000], lon[500000];
-float minDist[500000];
-bool  visit[500000];
+
+#define MAX_DIST  1000000000  //初始最大距离
+#define RATIO     2           //这个系数用来控制SPFA剪枝  
+#define BLOCK_NUM 18          //图中块的个数
+
+float minDist[ALL_NODES];
+bool  visit[ALL_NODES];
 queue<int> q;
+
+struct RoadNode {
+	int blockNum, blockOffset, borderNum, blockBorderNum;
+	float lat, lon;
+	vector<int> distToBorder;
+};
 
 struct NeighNode {
 	int id;
 	float dist;
 	NeighNode* next;
 };
-NeighNode* neighbour[500000];
-NeighNode* tail[500000];
+NeighNode* neighbour[ALL_NODES];
+NeighNode* tail[ALL_NODES];
+RoadNode roadNodes[ALL_NODES];
+
+vector<int> borderroadNodes[BLOCK_NUM];
 int n;
 
 
@@ -34,14 +46,22 @@ int n;
 void init() {
 
 	cout<<"Constructing the road..."<<endl;
-	ifstream fin("newNodes_long.txt");
-	string idTemp, latTemp, lonTemp;
+	ifstream fin("Nodes_with_block_infos.txt");
+	string idTemp, latTemp, lonTemp, blockNumTemp, blockOffsetTemp, borderNumTemp, blockBorderNumTemp;
 	int i = 0;
 
 	cout<<"Init..."<<endl;
-	while (fin>>idTemp>>latTemp>>lonTemp) {
-		lat[i] = atof(latTemp.c_str());
-		lon[i] = atof(lonTemp.c_str());
+	while (fin>>idTemp>>latTemp>>lonTemp>>blockNumTemp>>blockOffsetTemp>>borderNumTemp>>blockBorderNumTemp) {
+		//cout<<i<<endl;
+		roadNodes[i].lat = atof(latTemp.c_str());
+		roadNodes[i].lon = atof(lonTemp.c_str());
+		roadNodes[i].blockNum       = atoi(blockNumTemp.c_str());
+        roadNodes[i].blockOffset    = atoi(blockOffsetTemp.c_str());
+		roadNodes[i].borderNum      = atoi(borderNumTemp.c_str());
+		roadNodes[i].blockBorderNum = atoi(blockBorderNumTemp.c_str());
+		if (roadNodes[i].borderNum != -1) {
+			borderroadNodes[roadNodes[i].blockNum].push_back(i);
+		}
 		neighbour[i] = NULL;
 		tail[i] = NULL;
 		i++;
@@ -56,16 +76,18 @@ void init() {
 	while (wayIn>>ss>>tt) {
 		s = atoi(ss.c_str());
 		t = atoi(tt.c_str());
+        //cout<<s<<" "<<t<<endl;
 		if (neighbour[s] == NULL) {
 			neighbour[s]       = (struct NeighNode *)malloc(sizeof(NeighNode));
 			neighbour[s]->id   = t;
-			neighbour[s]->dist = Distance::getDist(lat[s], lon[s], lat[t], lon[t]);
+            //cout<<roadNodes[s].lat<<roadNodes[s].lon<<roadNodes[t].lat<<roadNodes[t].lon<<endl;
+			neighbour[s]->dist = Distance::getDist(roadNodes[s].lat, roadNodes[s].lon, roadNodes[t].lat, roadNodes[t].lon);
 			neighbour[s]->next = NULL;
 			tail[s]            = neighbour[s];
 		} else {
 			NeighNode *p  = (struct NeighNode*)malloc(sizeof(NeighNode)); 
 			p->id         = t;
-			p->dist       = Distance::getDist(lat[s], lon[s], lat[t], lon[t]);
+			p->dist       = Distance::getDist(roadNodes[s].lat, roadNodes[s].lon, roadNodes[t].lat, roadNodes[t].lon);
 			p->next       = NULL;
 			tail[s]->next = p;
 			tail[s]       = p;
@@ -89,6 +111,8 @@ void recycle() {
 		}
 		fout<<endl;
 	}
+
+	//delete(roadNodes);
 	fout.close();
 }
 
@@ -104,8 +128,7 @@ void initDist(int s) {
 /**
  * 因为路网转化成图之后是稀疏图，这里用SPFA算法求单源点最短路径
  */
-float SPFA(int s, int t) {
-	cout<<"SPFA..."<<endl;
+void SPFA(int s) {
 	initDist(s);
 	q.push(s);
 	int k;
@@ -118,8 +141,8 @@ float SPFA(int s, int t) {
 		while (p) {
 			if (minDist[k] + p->dist < minDist[p->id]) {
 				minDist[p->id] = minDist[k] + p->dist;
-				lineDist = Distance::getDist(lat[s], lon[s], lat[p->id], lon[p->id]);
-				if (!visit[p->id] && minDist[p->id] <= RATIO * lineDist) {
+				lineDist = Distance::getDist(roadNodes[s].lat, roadNodes[s].lon, roadNodes[p->id].lat, roadNodes[p->id].lon);
+				if (!visit[p->id] /*&& minDist[p->id] <= 4 * lineDist*/) {
 					visit[p->id] = true;
 					q.push(p->id);
 				}
@@ -129,7 +152,30 @@ float SPFA(int s, int t) {
 		}
 
 	}
-	return minDist[t];
+}
+
+void getAllDistToBorder() {
+	cout<<"get All Dist Border"<<endl;
+	//cout<<SPFA(46274, 13988)<<endl;
+	//int b;
+	//cin>>b;
+	ofstream fout("Dist_to_borders.txt");
+	for (int i = 0; i < ALL_NODES; i++) {
+		if (i >= 10000) break;
+	    fout<<i<<endl;
+		cout<<"Node: "<<i<<endl;
+		if (neighbour[i] == NULL) continue;
+		int label = roadNodes[i].blockNum;
+		SPFA(i);
+		for (int j = 0; j < borderroadNodes[label].size(); j++) {
+			fout<<minDist[borderroadNodes[label][j]]<<" ";
+		}
+		fout<<endl;
+		
+	}
+
+	fout.close();
+
 }
 
 int main() {
@@ -141,7 +187,7 @@ int main() {
 	time(&rawtime); 
 	timeinfo = localtime(&rawtime); 
 	printf ("系统时间是: %s", asctime (timeinfo) ); 
-	cout<<SPFA(46274, 46293)<<endl;
+	getAllDistToBorder();
 	time(&rawtime); 
 	timeinfo = localtime(&rawtime); 
 	printf ("系统时间是: %s", asctime (timeinfo) ); 
@@ -149,4 +195,7 @@ int main() {
 
 	return 0;
 }
+
+
+
 
